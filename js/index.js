281012fa3e1e8a37a3938506237a005c9033d278
aleupons <apahoquei@gmail.com>
2021-06-01 12:27:20 +0200

    Esborrar console.log i deshabilitar botons a l'encertar

--- js/index.js
+++ js/index.js
@@ -5,16 +5,15 @@ const numero = document.querySelector(".numero");
 const boto = document.querySelector(".adivinar");
 const missatge = document.querySelector(".mensaje");
 
-console.log(numeroAleatori);
-
 boto.addEventListener("click", (event) => {
   if (numero.value !== "") {
-    console.log(+numero.value);
     event.preventDefault();
     if (+numero.value === numeroAleatori) {
       missatge.classList.remove("error");
       missatge.classList.add("acierto");
       missatge.textContent = "El número ha sido acertado";
+      boto.disabled = true;
+      numero.disabled = true;
     } else {
       missatge.textContent =
         +numero.value < numeroAleatori
