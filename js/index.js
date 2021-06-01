e91f25c3b1011035c7d8e235242825c2f654daf4
aleupons <apahoquei@gmail.com>
2021-06-01 11:48:15 +0200

    Funcionalitat botó endevinar

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const generarNum = () => Math.floor(Math.random() * 10 + 1);
+
+const numeroAleatori = generarNum();
+const numero = document.querySelector(".numero");
+const boto = document.querySelector(".adivinar");
+const missatge = document.querySelector(".mensaje");
+
+console.log(numeroAleatori);
+
+boto.addEventListener("click", (event) => {
+  if (numero.value !== "") {
+    console.log(+numero.value);
+    event.preventDefault();
+    if (+numero.value === numeroAleatori) {
+      missatge.classList.remove("error");
+      missatge.classList.add("acierto");
+      missatge.textContent = "El número ha sido acertado";
+    } else {
+      missatge.textContent =
+        +numero.value < numeroAleatori
+          ? "El número tiene que ser mayor"
+          : "El número tiene que ser menor";
+      missatge.classList.remove("acierto");
+      missatge.classList.add("error");
+    }
+  } else {
+    missatge.classList.remove("error");
+    missatge.classList.remove("acierto");
+    missatge.textContent = "Introduzca un número";
+  }
+});
